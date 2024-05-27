996d4ba85bf6cfca0a9f22772926c48cc7b6db01
--global <site@jneris.com.br>
2024-05-27 19:02:47 -0300

    Add under maintenance

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export function App() {
+	return (
+		<main className="bg-neutral-900">
+			<div className="min-h-screen flex flex-col justify-center items-center">
+    			<img src="https://cdn-icons-png.flaticon.com/512/5178/5178953.png" alt="Logo" className="mb-8 h-40" />
+    			<h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-700 dark:text-white mb-4">
+					Site em <span className="text-[#FF637B]">construção</span>
+				</h1>
+    			<p className="text-center text-gray-500 dark:text-gray-300 text-lg md:text-xl lg:text-2xl mb-8">
+					Estamos trabalhando muito para melhorar a experiência do usuário. Fique atento!
+				</p>
+  			</div>
+		</main>
+	)
+}
